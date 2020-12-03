c405373e53e86b8aa1d6d36382538d7eb8714cca
Prinzivalle <riccardoprinzi1997@gmail.com>
2020-12-03 21:52:40 +0100

    fixed syncronization to generated shared secret of DH

--- Homework6/lab/pc3/root/receivePK.sh
+++ Homework6/lab/pc3/root/receivePK.sh
@@ -7,11 +7,3 @@ cd /root/keys
 
 #receive public keys of B and generate common secret with B
 (nc -l -p 9001 > /root/keys/dhpubPC2.pem && openssl pkeyutl -derive -inkey dhkeyPC3.pem -peerkey dhpubPC2.pem -out secret2.bin) &
-
-#cd /root/keys
-
-#generate common secret with A
-#openssl pkeyutl -derive -inkey dhkeyPC3.pem -peerkey dhpubPC1.pem -out secret1.bin
-
-#generate common secret with B
-#openssl pkeyutl -derive -inkey dhkeyPC3.pem -peerkey dhpubPC2.pem -out secret2.bin
